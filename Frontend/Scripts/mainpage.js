27941aa9379b7e91a434d8bebd84d6664e4082bb
ishanjoshi2000 <38184604+ishanjoshi2000@users.noreply.github.com>
2024-07-03 16:00:58 +0545

    final change

--- Frontend/Scripts/mainpage.js
+++ Frontend/Scripts/mainpage.js
@@ -1,11 +1,25 @@
 
-function scroller(direction) {
-    const storiesContainer = document.querySelector('.Stories');
-    const scrollAmount = 100; 
-    
-    if (direction === 'left') {
-        storiesContainer.scrollLeft -= scrollAmount;
-    } else if (direction === 'right') {
-        storiesContainer.scrollLeft += scrollAmount;
+const draw = document.getElementById('draw');
+
+
+draw.addEventListener('click', function() {
+   
+    const icon = draw.querySelector('i');
+    if (icon.classList.contains('fa-angle-right')) {
+        icon.classList.remove('fa-angle-right');
+        icon.classList.add('fa-angle-left');
+    } else {
+        icon.classList.remove('fa-angle-left');
+        icon.classList.add('fa-angle-right');
     }
-}
+
+    const toggle = document.querySelector('.exlpore-section');
+
+    if (toggle.style.display === 'none' ) {
+        toggle.style.display = 'block'; 
+
+       
+    } else {
+        toggle.style.display = 'none'; 
+    }
+});
